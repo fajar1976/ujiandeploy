0d9a59313f63671ba5b9f3ff63e400c4817c9fb0
Fajar Pahmi Padilah <fajarpahmi.2021@student.uny.ac.id>
2023-05-27 13:44:37 +0700

    bisa dong

--- online_voting/connection.php
+++ online_voting/connection.php
@@ -1,7 +1,14 @@
 <?php
 error_reporting(1);
-mysqli_connect('mysql', 'user', 'root') or die(mysqli_error( $mysqli));
-mysqli_select_db('poll') or die(mysqli_error( $mysqli));
+const DB_HOST = 'mysql';
+const DB_USER = 'user';
+const DB_PASSWORD = 'root';
+const DB_NAME     = 'poll';
 
+$dbcon = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+if($dbcon->connect_error){
+	die("Database connection failed".$dbcon->connect_error);
+}
 
 ?>
