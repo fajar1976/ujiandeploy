f7ee4303712c68865f8edb0a5aca435a7544fa8a
Fajar Pahmi Padilah <fajarpahmi.2021@student.uny.ac.id>
2023-05-27 14:48:51 +0700

    bisa

--- manajemen/includes/dbconnection.php
+++ manajemen/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("mysql", "root", "root", "ccmsdb");
+$con=mysqli_connect("imsql", "root", "root", "ccmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
